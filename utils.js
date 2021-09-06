3a90f26bea723d4c9c03943e30affd2f68d561b1
Orlando Neto <orlandoneto23@gmail.com>
2021-09-06 13:47:52 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,10 @@
+// Bloco de código que válida se uma senha é forte de 8 caracteres
+// Pode ser reaproveitado a validação via lib yup como o regex de senha forte. 
+password: Yup.string()
+.required('Campo obrigatório')
+.matches(
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+  'Deve conter 8 caracteres, uma maiúscula, uma minúscula, um número e um caractere especial.'
+),
+
 
